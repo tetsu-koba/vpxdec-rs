53eadf846b712ef6d78ceeb17c8f599650541b7a
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-08-29 17:55:28 +0900

    Split do_frame()

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use libivf_rs as ivf;
 use std::env;
 use std::error::Error;
 use std::fs::{File, OpenOptions};
+use std::io;
 use std::io::{ErrorKind, Write};
 mod vpxdec;
+
 fn decode(
     input_file: &str,
     output_file: &str,
@@ -21,76 +23,64 @@ fn decode(
         return Err("Video size mismatch".into());
     }
 
-    let mut frame_index = 0;
     let mut frame_buffer = Vec::<u8>::new();
     let fourcc = reader.header.fourcc;
     let mut vpxdec = vpxdec::VpxDec::init(&fourcc)?;
 
-    'outer: loop {
-        let frame_header = match reader.read_ivf_frame_header() {
-            Ok(f) => f,
-            Err(ref e) if e.kind() == ErrorKind::UnexpectedEof => break,
-            Err(e) => {
+    let mut frame_index = 0;
+    loop {
+        if let Err(e) = do_frame(&mut reader, &mut vpxdec, &mut frame_buffer, &mut outfile) {
+            if let Some(io_err) = e.downcast_ref::<io::Error>() {
+                match io_err.kind() {
+                    ErrorKind::UnexpectedEof | ErrorKind::BrokenPipe => {}
+                    _ => eprintln!("Error: {e:?}"),
+                }
+            } else {
                 eprintln!("Error: {e:?}");
-                break;
             }
-        };
-        let frame_size: usize = frame_header.frame_size as _;
-        if frame_buffer.len() < frame_size {
-            frame_buffer.resize(frame_size, 0);
-        }
-        match reader.read_frame(&mut frame_buffer[..frame_size]) {
-            Ok(_) => {}
-            Err(ref e) if e.kind() == ErrorKind::UnexpectedEof => break,
-            Err(e) => {
-                eprintln!("Error: {e:?}");
-                break;
-            }
-        }
-        if let Err(e) = vpxdec.decode(&frame_buffer[..frame_size]) {
-            eprintln!("Error: {e:?}");
             break;
         }
-        loop {
-            let img = vpxdec.get_frame();
-            if img == 0 as _ {
-                break;
-            }
-            unsafe {
-                let img = *img;
-                let mut ptr = img.planes[0];
-                for _ in 0..img.d_h {
-                    match outfile.write_all(std::slice::from_raw_parts(ptr, img.d_w as _)) {
-                        Ok(_) => {}
-                        Err(ref e) if e.kind() == ErrorKind::BrokenPipe => break 'outer,
-                        Err(e) => {
-                            eprintln!("Error: {e:?}");
-                            break 'outer;
-                        }
-                    }
-                    ptr = ptr.add(img.stride[0] as _);
-                }
-                for i in 1..3 {
-                    let mut ptr = img.planes[i];
-                    for _ in 0..img.d_h / 2 {
-                        match outfile.write_all(std::slice::from_raw_parts(ptr, (img.d_w / 2) as _))
-                        {
-                            Ok(_) => {}
-                            Err(ref e) if e.kind() == ErrorKind::BrokenPipe => break 'outer,
-                            Err(e) => {
-                                eprintln!("Error: {e:?}");
-                                break 'outer;
-                            }
-                        }
-                        ptr = ptr.add(img.stride[i] as _);
-                    }
-                }
-            }
-            frame_index += 1;
-        }
+        frame_index += 1;
     }
 
     _ = frame_index;
+    Ok(())
+}
+
+fn do_frame(
+    reader: &mut ivf::IvfReader,
+    vpxdec: &mut vpxdec::VpxDec,
+    frame_buffer: &mut Vec<u8>,
+    outfile: &mut File,
+) -> Result<(), Box<dyn Error>> {
+    let frame_header = reader.read_ivf_frame_header()?;
+    let frame_size: usize = frame_header.frame_size as _;
+    if frame_buffer.len() < frame_size {
+        frame_buffer.resize(frame_size, 0);
+    }
+    reader.read_frame(&mut frame_buffer[..frame_size])?;
+    vpxdec.decode(&frame_buffer[..frame_size])?;
+    loop {
+        let img = vpxdec.get_frame();
+        if img == 0 as _ {
+            break;
+        }
+        unsafe {
+            let img = *img;
+            let mut ptr = img.planes[0];
+            for _ in 0..img.d_h {
+                outfile.write_all(std::slice::from_raw_parts(ptr, img.d_w as _))?;
+                ptr = ptr.add(img.stride[0] as _);
+            }
+            for i in 1..3 {
+                let mut ptr = img.planes[i];
+                for _ in 0..img.d_h / 2 {
+                    outfile.write_all(std::slice::from_raw_parts(ptr, (img.d_w / 2) as _))?;
+                    ptr = ptr.add(img.stride[i] as _);
+                }
+            }
+        }
+    }
     Ok(())
 }
 
